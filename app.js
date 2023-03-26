d295798918421763b9e66f77acf71b68ac64d148
Meraki_DNA <dononichols@gmail.com>
2023-03-26 19:33:31 -0400

    JS-Challenge #3

---
+++ app.js
@@ -0,0 +1,24 @@
+// Challenge #3 
+
+// Operators: Boolean & Logical
+/*
+
+There are two gymnatics teams, dolphins and Koalas.
+They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+1. Calculate the average score for each team, using the test data below.
+
+2. Compare the teams average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Inclued a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a scroe of at least 100 points. 
+HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks
+
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: 
+-Dolphins score 96, 108, and 89.
+-Koalas scrore 88, 91, 110.
+
+
+
+*/
